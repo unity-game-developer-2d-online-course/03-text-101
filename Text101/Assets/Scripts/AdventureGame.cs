774730305df72315721b0a0a217ead1adf4a13e7
Mattias Strömberg <m.stromberg@outlook.com>
2020-08-20 10:46:42 +0200

    Update text component

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Text101
+{
+    public class AdventureGame : MonoBehaviour
+    {
+        [SerializeField] private Text _textComponent;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _textComponent.text = "I am added programmatically!";
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
---
+++ Text101/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~M:Text101.AdventureGame.Start")]
+[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~M:Text101.AdventureGame.Update")]
