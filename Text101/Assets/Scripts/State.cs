7a8161be1265f8bc8cf0b7a7007e02d4dfe23b03
Mattias Strömberg <m.stromberg@outlook.com>
2020-10-13 17:43:43 +0200

    Unity scriptable objects

---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Text101
+{
+    [CreateAssetMenu(menuName = "State")]
+    public class State : ScriptableObject
+    {
+        [TextArea(14, 10)] [SerializeField] private string _storyText;
+    }
+}
